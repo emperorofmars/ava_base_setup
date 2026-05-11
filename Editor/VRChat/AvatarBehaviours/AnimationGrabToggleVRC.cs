c7010d08a96290bf657acb5b4756bcdf76bb7f9c
Mars <mars@squirrel.pub>
2026-05-11 19:09:33 +0200

    add grab collider behaviour

---
+++ Editor/VRChat/AvatarBehaviours/AnimationGrabToggleVRC.cs
@@ -0,0 +1,21 @@
+#if UNITY_EDITOR
+#if AVA_BASE_SETUP_VRCHAT
+
+using UnityEngine;
+using VRC.SDK3.Dynamics.Contact.Components;
+using VRC.SDK3.Dynamics.PhysBone.Components;
+
+namespace com.squirrelbite.ava_base_setup.vrchat
+{
+	[AddComponentMenu("AVA/VRChat/Behaviours/AnimationGrabToggle")]
+	[HelpURL("https://codeberg.org/emperorofmars/ava_base_setup")]
+	public class AnimationGrabToggleVRC : AnimationToggleVRC
+	{
+		public VRCContactReceiver Contact;
+		public VRCPhysBoneCollider Collider;
+		public string Hand = "both";
+	}
+}
+
+#endif
+#endif
